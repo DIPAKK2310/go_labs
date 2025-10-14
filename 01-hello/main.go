3b33d5c0e62f25ca8beec4e1a89c6bf45df3763d
DIPAKK2310 <dipakkhare217@gmail.com>
2025-10-14 16:27:18 +0000

    Feat(01-hello):Add

---
+++ 01-hello/main.go
@@ -0,0 +1,8 @@
+package main
+// main  package is a way to group functions, and it's made up of all the files in the same directory
+import "fmt"
+// This package is one of the standard library packages you got when you installed Go.
+func main()  {
+	fmt.Println("Hello from go")
+}
+	
---
+++ README.md
@@ -0,0 +1,2 @@
+# Go Labs
+### This is a study guide to learn go lang by doing stuff
