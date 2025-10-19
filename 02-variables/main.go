becf78af15376a88bb8bf65a6fd5256b66c3dbe3
DIPAKK2310 <dipakkhare217@gmail.com>
2025-10-19 16:49:52 +0000

    Feat(Use variables):Use

--- 02-variables/main.go
+++ 02-variables/main.go
@@ -1,25 +1,9 @@
 package main
+
 import "fmt"
 
-func main(){
-	var userName = "Dipak Khare"
-	fmt.Println(userName)
-	//%T format specifier to print type of variable
-	fmt.Printf("Data type of variable is %T \n " ,userName)
-	
-	var isLoggedIn = true
-	fmt.Println(isLoggedIn)
-	//%T format specifier to print type of variable
-	fmt.Printf("Data type of variable is  %T \n" ,isLoggedIn)
-
-	//var smallFloat = 32.78656 is written by shorthand ":=" it detect data type also only in function not on pakcge level
-	 smallFloat:= 32.78656
-	fmt.Println(smallFloat)
-	//%T format specifier to print type of variable
-	fmt.Printf("Data type of variable is: %T \n" ,smallFloat)
-
-	var anotherVariable int
-	fmt.Println(anotherVariable)
-	fmt.Printf("data type of variable is: %T \n", anotherVariable)
-	//Lets go with go lang , vid-6=:11:12
+func main()  {
+ var userName string = "Dipak"	
+ var age int = 23
+ fmt.Println(age,userName)
 }
